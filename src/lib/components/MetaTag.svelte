c412ee514c514c8eb0cad249fa19a18c25e12ebe
sshawn <github.mlxup@simplelogin.com>
2025-07-03 22:20:10 +0800

    metatag

---
+++ src/lib/components/MetaTag.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { MetaTags } from 'svelte-meta-tags';
+
+	const url = $page.url.origin;
+
+	let { customMeta = {} } = $props();
+	const meta = {
+		title: 'NeoNote',
+		desc: 'Fastest way to publish markdown Neovim',
+		url: $page.url.href,
+		image: url + '/logo.png',
+		twitter: '',
+		twitterImage: '',
+		...customMeta
+	};
+</script>
+
+<MetaTags
+	title={meta.title}
+	titleTemplate={`${meta.title}: ${meta.desc}`}
+	description={meta.desc}
+	canonical={meta.url}
+	openGraph={{
+		url: meta.url,
+		title: meta.title,
+		description: meta.desc,
+		type: 'website',
+		images: [
+			{
+				url: meta.image,
+				width: 800,
+				height: 600,
+				alt: meta.title
+			}
+		]
+	}}
+	twitter={{
+		handle: meta.twitter,
+		site: meta.twitter,
+		cardType: 'summary_large_image',
+		title: meta.title,
+		description: meta.desc,
+		image: meta.twitterImage,
+		imageAlt: meta.titleeovi
+	}}
+/>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,8 @@
 	import '../app.css';
 
 	let { children } = $props();
+	import MetaTag from '$lib/components/MetaTag.svelte';
 </script>
 
+<MetaTag></MetaTag>
 {@render children()}
