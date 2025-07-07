1c11219d26b8c19eb5ec14cff94b86b194ed8796
sshawn <github.mlxup@simplelogin.com>
2025-07-07 12:14:42 +0800

    hide navbar and feedback in public notes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -50,36 +50,40 @@
 </svelte:head>
 
 <div class="flex min-h-screen flex-col">
-	<header class="navbar shadow-sm md:px-8">
-		<div class="flex flex-1">
-			<a href="/" role="button" class="flex gap-2">
-				<Pencil class="h-8 w-8 text-blue-400" />
-				<span class="text-2xl font-bold text-white">NeoNote</span>
-			</a>
-		</div>
-		<div class="flex items-center gap-2">
-			<a
-				href="/notes"
-				role="button"
-				class={page.url.pathname.startsWith('/notes')
-					? 'btn btn-ghost text-gray-600'
-					: 'btn btn-ghost text-white'}
-			>
-				Notes
-			</a>
-			<a
-				href="/account"
-				role="button"
-				class={page.url.pathname.startsWith('/account') || page.url.pathname.startsWith('/login')
-					? 'text-gray-600'
-					: 'text-white'}
-			>
-				<CircleUser class="h-8 w-8" />
-			</a>
-		</div>
-	</header>
+	{#if page.route.id !== '/(public)/[id]'}
+		<header class="navbar shadow-sm md:px-8">
+			<div class="flex flex-1">
+				<a href="/" role="button" class="flex gap-2">
+					<Pencil class="h-8 w-8 text-blue-400" />
+					<span class="text-2xl font-bold text-white">NeoNote</span>
+				</a>
+			</div>
+			<div class="flex items-center gap-2">
+				<a
+					href="/notes"
+					role="button"
+					class={page.url.pathname.startsWith('/notes')
+						? 'btn btn-ghost text-gray-600'
+						: 'btn btn-ghost text-white'}
+				>
+					Notes
+				</a>
+				<a
+					href="/account"
+					role="button"
+					class={page.url.pathname.startsWith('/account') || page.url.pathname.startsWith('/login')
+						? 'text-gray-600'
+						: 'text-white'}
+				>
+					<CircleUser class="h-8 w-8" />
+				</a>
+			</div>
+		</header>
+	{/if}
 	<main class="flex-1">
 		{@render children()}
 	</main>
-	<FeedbackWidget bind:message bind:email bind:name />
+	{#if page.route.id !== '/(public)/[id]'}
+		<FeedbackWidget bind:message bind:email bind:name />
+	{/if}
 </div>
