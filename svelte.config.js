26ca0d68c33041d0ef979590ab3015ff26054d80
sshawn <github.mlxup@simplelogin.com>
2026-01-18 19:39:29 +0800

    chore: pnpm update

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,12 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: { adapter: adapter() },
+	vitePlugin: {
+		inspector: {
+			toggleKeyCombo: 'alt-g'
+		}
+	}
 };
 
 export default config;
